19aefc6807e6ec7c307d1c085aab83e218e29b14
Ubuntu <ubuntu@ec037-030.maas>
2020-07-16 10:05:58 +0000

    add training data number in linear case

--- lab1/main.py
+++ lab1/main.py
@@ -5,7 +5,7 @@ from net import FC_Net
 
 # train linear
 net_linear = FC_Net(5, 5)
-x_train, y_train = generate_linear()
+x_train, y_train = generate_linear(500)
 net_linear.train(x_train, y_train, epochs=50000, learning_rate=0.05)
 
 # test linear
