73ea507a721a7f536526bfeeecb71d731516953c
Ubuntu <ubuntu@ec037-030.maas>
2020-07-17 16:51:46 +0000

    add with_sigmoid argument to the constructor

--- lab1/net.py
+++ lab1/net.py
@@ -2,9 +2,10 @@ import numpy as np
 from utils import cal_accuracy
 
 class FC_Net:
-    def __init__(self, first_hidden_width, second_hidden_width):
+    def __init__(self, first_hidden_width, second_hidden_width, with_sigmoid=True):
         input_width = 2
         output_width = 1
+        self.with_sigmoid = with_sigmoid
         
         # random generates weights
         self.W = []
@@ -51,7 +52,10 @@ class FC_Net:
 
         for i in range(len(self.W)):
             self.z.append(np.matmul(self.a[i], self.W[i]))
-            self.a.append(self.sigmoid(self.z[i]))
+            if (not self.with_sigmoid) and i < (len(self.W) - 1):
+                self.a.append(self.z[i])
+            else:
+                self.a.append(self.sigmoid(self.z[i]))
 
         return self.a[-1]
 
@@ -61,7 +65,10 @@ class FC_Net:
 
         for i in range(len(self.W) - 1, -1, -1):
             if (i - 1) >= 0:
-                deltas[i - 1] = np.matmul(deltas[i], self.W[i].T) * self.derivative_sigmoid(self.z[i - 1])
+                if self.with_sigmoid:
+                    deltas[i - 1] = np.matmul(deltas[i], self.W[i].T) * self.derivative_sigmoid(self.z[i - 1])
+                else:
+                    deltas[i - 1] = np.matmul(deltas[i], self.W[i].T)
 
             # update weights
             gradient_to_w = np.matmul(self.a[i].T, deltas[i])
